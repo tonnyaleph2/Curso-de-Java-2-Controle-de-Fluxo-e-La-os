8430ef4c48e8cbb0d395350a38d5538ec4579bcc
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-07-28 14:56:11 -0300

    Create Contador.java

---
+++ Contador.java
@@ -0,0 +1,12 @@
+public class contador {
+  public static void main(String[]args){
+    int contador=0;
+    
+    System.out.println("Este programa conta ate 10");
+       
+    while(contador<10){
+      contador++;
+      System.out.println("Contagem:" + contador);
+    }
+  }
+}
