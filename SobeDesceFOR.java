e812270e5749498a7e20694478a8ff6a343aef30
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-07-29 10:58:26 -0300

    Create SobeDesceFOR.java

---
+++ SobeDesceFOR.java
@@ -0,0 +1,8 @@
+public class exemplofor{
+  public static void main(String[]args){
+    for(int sobe=1,desce=10;sobe<=10 && desce>=1;sobe++,desce--){
+      System.out.printf("%d \t %d \n", sobe,desce);
+    }
+  }
+}
+
